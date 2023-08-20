7fa8bbc1f9ade24fd0008e75e29366c778df4192
Vignesh-Kumaar-S <142718033+Vignesh-Kumaar-S@users.noreply.github.com>
2023-08-20 18:23:10 +0530

    Add files via upload
    Day 1 Class session programs.

---
+++ first1/Datatype1.java
@@ -0,0 +1,25 @@
+package day1.first1;
+
+public class Datatype1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String stockname = "SBI";
+Long stockNumber = 12345678345L;
+int openprice = 70;
+Float currentMarketValue = 72.25f;
+boolean buy = true;
+char shortform = 'S';
+Double closePrice = 71.199;
+
+System.out.println("Stock name is : " +stockname);
+System.out.println("Today's open price is : " +openprice);
+System.out.println("Current market rate : " +currentMarketValue);
+System.out.println("Is best to buy : " +buy);
+System.out.println("Expected today's close price : " +buy);
+System.out.println("Is best to buy : " +buy);
+System.out.println("Is best to buy : " +buy);
+
+	}
+
+}
